ce83c0b5ad9ab6700e10efc1f3474c61055af9d7
Vasishath Kaushal <vasishath@gmail.com>
2021-06-12 18:43:32 +0530

    Revert "Alioth: Build Kernel"
    This reverts commit caf2f3045889bc5ae048bdacdd470cdf140d1e2b.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,9 +20,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 # Prebuilt Core
 
 # Kernel
-TARGET_KERNEL_SOURCE := kernel/xiaomi/alioth
-TARGET_KERNEL_CONFIG := vendor/alioth_defconfig
 TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
-
+TARGET_KERNEL_CONFIG := vendor/alioth_defconfig
 
